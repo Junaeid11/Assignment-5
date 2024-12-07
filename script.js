5a5e305608e98e80863f65bc626acde9c705568e
junaeid <junaeidahmed979@gmail.com>
2024-12-07 16:25:44 +0600

    add all function

--- script.js
+++ script.js
@@ -1,60 +1,114 @@
-// let balance = 100;
-
-document.getElementById("donation").addEventListener('click', ()=> addOrHideSection("donation"));
-document.getElementById("history").addEventListener('click', ()=>addOrHideSection('history'));
+document
+  .getElementById("donation")
+  .addEventListener("click", () => addOrHideSection("donation"));
+document
+  .getElementById("history")
+  .addEventListener("click", () => addOrHideSection("history"));
 
 // {i used ternary operator}
 addOrHideSection = (section) => {
-    const donationSection = document.getElementById('donationSection');
-    const historySection = document.getElementById('historySection');
-  
-    section === 'donation'
-      ? (donationSection.classList.remove('hidden'), historySection.classList.add('hidden'))
-      : (historySection.classList.remove('hidden'), donationSection.classList.add('hidden'));
-  };
- 
+  const donationSection = document.getElementById("donationSection");
+  const historySection = document.getElementById("historySection");
 
-// function addTask(button) {
-//     const model = document.getElementById("my_modal_1");
-//     const taskBox = document.getElementById("historySection");
-//     const parentDiv = button.closest(".flex");
-//     const title = parentDiv.querySelector(".content").innerHTML;
-//     const input = parentDiv.querySelector(".donation");
-//     const data = input.value.trim();
-//     if (data === '' || isNaN(data)) {
-//         model.close();
-//         return;
-//     }
-//     const now = new Date();
-//     const date = now.toUTCString();
-//     const time = now.toLocaleTimeString();
-//     const li = document.createElement('p');
-//     li.className = 'p-7 my-5 text-black bg-gray-200 rounded-xl shadow';
-//     li.innerHTML = `<span class="text-2xl font-bold">${data} taka Donated ${title}</span> 
-//                     <p>Date: ${date} at Time: ${time}</p>`;
-    
-//     taskBox.appendChild(li);
-//     input.value = ''; 
-//     model.open();
-// }
-
-document.getElementById('btn-add-money').addEventListener('click', (event)=>{
-     event.preventDefault();
-    const addMoney = parseFloat(document.getElementById('input-add-money').value);
-    if(!isNaN(addMoney)){
-        const credit = parseFloat(document.getElementById('total-balance').innerText);
-        const ultimateCredit = credit + addMoney;
-        document.getElementById('total-balance').innerText = ultimateCredit;
-         const debitBal = parseFloat(document.getElementById('balance').innerText);
-         const ultimateDeb =  debitBal - addMoney;
-        document.getElementById('balance').innerText = ultimateDeb;
-
+  section === "donation"
+    ? (donationSection.classList.remove("hidden"),
+      historySection.classList.add("hidden"))
+    : (historySection.classList.remove("hidden"),
+      donationSection.classList.add("hidden"));
+};
+document.getElementById("btn-add-money").addEventListener("click", (event) => {
+  event.preventDefault();
+  const addMoney = parseFloat(document.getElementById("input-add-money").value);
+  const debitBal = parseFloat(document.getElementById("balance").innerText);
+  if (!isNaN(addMoney)) {
+    const ultimateDeb = debitBal - addMoney;
+    if (ultimateDeb < 0) {
+      return alert("You don't have enough balance to donate");
+    } else {
+      const model = document.getElementById("model-pop");
+      model.showModal();
+      const credit = parseFloat(
+        document.getElementById("total-balance").innerText
+      );
+      const ultimateCredit = credit + addMoney;
+      document.getElementById("total-balance").innerText = ultimateCredit;
+      document.getElementById("balance").innerText = ultimateDeb;
+      const div = document.createElement("div");
+      const date = new Date();
+      const time = date.toLocaleTimeString();
+      const title = document.getElementsByClassName("content")[0].innerHTML;
+      div.className = " justify-between p-2 bg-slate-100 rounded-xl my-2";
+      div.innerHTML = `<span class="text-2xl text-black font-bold">${addMoney} taka is Donated for ${title}</span>
+                    <p class='text-gray-400'>Date: ${date} at Time: ${time}</p>`;
+      document.getElementById("historySection").appendChild(div);
+      return;
     }
-    else{
-        return alert('Please enter a valid number');
+  } else {
+    return alert("Please enter a valid number");
+  }
+});
+document.getElementById("btn-add-money2").addEventListener("click", (event) => {
+  event.preventDefault();
+  const addMoney = parseFloat(
+    document.getElementById("input-add-money2").value
+  );
+  const debitBal = parseFloat(document.getElementById("balance").innerText);
+  if (!isNaN(addMoney)) {
+    const ultimateDeb = debitBal - addMoney;
+    if (ultimateDeb < 0) {
+      return alert("You don't have enough balance to donate");
     }
-})
+    const model = document.getElementById("model-pop");
+    model.showModal();
+    const credit = parseFloat(
+      document.getElementById("total-balance2").innerText
+    );
+    const ultimateCredit = credit + addMoney;
+    document.getElementById("total-balance2").innerText = ultimateCredit;
+    document.getElementById("balance").innerText = ultimateDeb;
+    const div = document.createElement("div");
+    const date = new Date();
+    const time = date.toLocaleTimeString();
+    const title = document.getElementsByClassName("content2")[0].innerHTML;
+    div.className = " justify-between p-2 bg-slate-100 rounded-xl my-2";
+    div.innerHTML = `<span class="text-2xl text-black font-bold">${addMoney} taka is Donated for ${title}</span>
+                    <p class='text-gray-400'>Date: ${date} at Time: ${time}</p>`;
+    document.getElementById("historySection").appendChild(div);
+    return;
+  } else {
+    return alert("Please enter a valid number");
+  }
+});
+document.getElementById("btn-add-money3").addEventListener("click", (event) => {
+  event.preventDefault();
+  const addMoney = parseFloat(
+    document.getElementById("input-add-money3").value
+  );
+  const debitBal = parseFloat(document.getElementById("balance").innerText);
+  if (!isNaN(addMoney)) {
+    const ultimateDeb = debitBal - addMoney;
+    if (ultimateDeb < 0) {
+      return alert("You don't have enough balance to donate");
+    }
+    const model = document.getElementById("model-pop");
+    model.showModal();
+    const credit = parseFloat(
+      document.getElementById("total-balance3").innerText
+    );
+    const ultimateCredit = credit + addMoney;
+    document.getElementById("total-balance3").innerText = ultimateCredit;
 
-
-
-
+    document.getElementById("balance").innerText = ultimateDeb;
+    const div = document.createElement("div");
+    const date = new Date();
+    const time = date.toLocaleTimeString();
+    const title = document.getElementsByClassName("content3")[0].innerHTML;
+    div.className = " justify-between p-2 bg-slate-100 rounded-xl my-2";
+    div.innerHTML = `<span class="text-2xl text-black font-bold">${addMoney} taka is Donated for ${title}</span>
+                    <p class='text-gray-400'>Date: ${date} at Time: ${time}</p>`;
+    document.getElementById("historySection").appendChild(div);
+    return;
+  } else {
+    return alert("Please enter a valid number");
+  }
+});
