626a80757a81a7f6eb144ee11146a7fa5db985ea
junaeid <junaeidahmed979@gmail.com>
2024-12-07 13:41:06 +0600

    add this file

---
+++ script.js
@@ -0,0 +1,42 @@
+let balance = 100;
+
+document.getElementById("donation").addEventListener('click', ()=> addOrHideSection("donation"));
+document.getElementById("history").addEventListener('click', ()=>addOrHideSection('history'));
+
+// {i used ternary operator}
+addOrHideSection = (section) => {
+    const donationSection = document.getElementById('donationSection');
+    const historySection = document.getElementById('historySection');
+  
+    section === 'donation'
+      ? (donationSection.classList.remove('hidden'), historySection.classList.add('hidden'))
+      : (historySection.classList.remove('hidden'), donationSection.classList.add('hidden'));
+  };
+ 
+
+function addTask(button) {
+    const model = document.getElementById("my_modal_1");
+    const taskBox = document.getElementById("historySection");
+    const parentDiv = button.closest(".flex");
+    const title = parentDiv.querySelector(".content").innerHTML;
+    const input = parentDiv.querySelector(".donation");
+    const data = input.value.trim();
+    if (data === '' || isNaN(data)) {
+        model.close();
+        return;
+    }
+    const now = new Date();
+    const date = now.toUTCString();
+    const time = now.toLocaleTimeString();
+    const li = document.createElement('p');
+    li.className = 'p-7 my-5 text-black bg-gray-200 rounded-xl shadow';
+    li.innerHTML = `<span class="text-2xl font-bold">${data} taka Donated ${title}</span> 
+                    <p>Date: ${date} at Time: ${time}</p>`;
+    
+    taskBox.appendChild(li);
+    input.value = ''; 
+    model.open();
+}
+
+
+
